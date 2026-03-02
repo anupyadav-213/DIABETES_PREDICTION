24ac262c0fb95f4f2f5e2b15ecd745bfe04208d6
anupyadav-213 <anupyadav1302@gmail.com>
2026-03-02 22:41:57 +0530

    Updated backend URL

--- diabetes-frontend/src/App.js
+++ diabetes-frontend/src/App.js
@@ -26,7 +26,7 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    const response = await fetch("https://diabetes-prediction-r3hz.onrender.com/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
